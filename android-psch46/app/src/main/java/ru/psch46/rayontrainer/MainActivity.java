d32aab94c0e5d451758c0ea30d6d112f81699e2e
Ayuemin <105455063+Ayuemin@users.noreply.github.com>
2026-09-03 20:45:58 +0300

    Load trainer only from bundled assets

--- android-psch46/app/src/main/java/ru/psch46/rayontrainer/MainActivity.java
+++ android-psch46/app/src/main/java/ru/psch46/rayontrainer/MainActivity.java
@@ -6,34 +6,43 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
     private WebView webView;
-    private static final String START_URL = "https://psch46-rayon-trainer.ayuemin.chatgpt.site";
+    private static final String START_URL = "file:///android_asset/www/index.html";
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setStatusBarColor(Color.rgb(13, 32, 51));
+        getWindow().setNavigationBarColor(Color.rgb(13, 32, 51));
         getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_FULLSCREEN |
                 View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
         webView = new WebView(this);
-        webView.setBackgroundColor(Color.WHITE);
-        webView.setWebViewClient(new WebViewClient());
+        webView.setBackgroundColor(Color.rgb(232, 237, 240));
         webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return request.getUrl() == null || !"file".equals(request.getUrl().getScheme());
+            }
+        });
 
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
         settings.setDatabaseEnabled(true);
         settings.setLoadsImagesAutomatically(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(false);
+        settings.setBlockNetworkLoads(true);
         settings.setUseWideViewPort(true);
         settings.setLoadWithOverviewMode(false);
         settings.setBuiltInZoomControls(false);
